d3fdb514f43e4745cbfb977e7f016ae4a149bb26
mohammadreza 2104 <mohammadrezawolf2104@gmail.com>
2022-11-16 15:18:18 +0330

    for testing the author name!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <Stack gap="40px" alignItems="center" px="40px" pt="24px">
                 <img src={Logo} alt="logo" width="200px" height="40px"/>
                 <Typography variant="h5" pb="40px" mt="20px">
-                    Made with Love by MohammadReza
+                    Made with Love by MohammadReza2104
                 </Typography>
 
             </Stack>
